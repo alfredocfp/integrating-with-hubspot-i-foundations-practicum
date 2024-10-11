9d135e334266b5de2f8af2005861453489a88cb2
alfcortes <alfcortes@palaceresorts.com>
2024-10-11 17:36:30 -0600

    commit de practica

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-
 app.set('view engine', 'pug');
+app.set('views', './views');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,16 +11,66 @@ app.use(express.json());
 const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
+app.get('/', async (req, res) => {
+    const body = {
+        properties: [
+          'dealname',
+          'closedate',
+          'hubspot_owner_id',
+          'amount',
+        ],
+      };
+    const url = 'https://api.hubspot.com/crm/v3/objects/deals';
+    const config = {
+        headers: { 'content-type': 'application/json', Authorization: `Bearer ${PRIVATE_APP_ACCESS}` },
+        data: JSON.stringify(body),
+      };
+    try {
+        const resp = await axios.get(url, config);
+        const data = resp.data.results;
+        console.log(data);
+        res.render('homepage', { title: 'Deals | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
 // * Code for Route 1 goes here
-
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
 // * Code for Route 2 goes here
-
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+app.post('/postdeals', async (req, res) => {
+    const update = {
+        properties: {
+            "dealname": req.body.newVal,
+            "amount": req.body.amount    
+        }
+    }
+    console.log(req.body)
+    const dealId = req.body.dealId;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        if (!dealId) {
+            // Crear un nuevo deal
+            const createDealUrl = 'https://api.hubapi.com/crm/v3/objects/deals';
+            console.log('Creando nuevo deal:', update);
+            await axios.post(createDealUrl, update, { headers });
+        } else {
+            // Actualizar el deal existente
+            const updateDeal = `https://api.hubapi.com/crm/v3/objects/deals/${dealId}`;
+            console.log('Actualizando deal:', dealId, update);
+            await axios.patch(updateDeal, update, { headers });
+        }
+        res.redirect('back');
+    } catch(err) {
+        console.error(err);
+    }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
